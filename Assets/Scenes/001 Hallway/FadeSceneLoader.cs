475942247c81c69c6d1680daa4e121933cc5c431
Tor-Arne Sandstrak <github@logi.pm>
2025-03-05 11:53:34 +0100

    Added Clock to the hallway scene.

--- Assets/Scenes/001 Hallway/FadeSceneLoader.cs
+++ Assets/Scenes/001 Hallway/FadeSceneLoader.cs
@@ -1,19 +1,26 @@
+using System;
 using Eflatun.SceneReference;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class FadeSceneLoader : MonoBehaviour {
     [SerializeField] private SceneReference _sceneToLoad;
     [SerializeField] private float _fadeTime = 2f;
-    [SerializeField] private AnimationCurve _fadeCurve;
-    [SerializeField] private SpriteRenderer _fadeOverlay;
 	[SerializeField] private float _fadeStartDistance = 0.4f;
 	
 	private bool _checkFadeDistance = false; 
 	private float _fadeMaxByTime = -1f;
 	private PlayerController _playerController;
 
+	private FadeController _fadeController;
 	private void Awake() {
 		_playerController = GameObject.FindAnyObjectByType<PlayerController>();
+		_fadeController = Object.FindFirstObjectByType<FadeController>();
+		FadeController.OnFadeComplete += SceneChange;
+	}
+
+	private void OnDestroy() {
+		FadeController.OnFadeComplete -= SceneChange;
 	}
 
 	private void Update() {
@@ -24,33 +31,15 @@ public class FadeSceneLoader : MonoBehaviour {
 			_fadeMaxByTime = Time.realtimeSinceStartup + _fadeTime;
 			return;
 		}
-		FadeByTime(_fadeMaxByTime);
+		_fadeController.StartFade(_fadeMaxByTime,_fadeTime);
 	}
 
-	private void FadeByTime(float fadebyTime) {
-		
-
-		if (Time.realtimeSinceStartup <= fadebyTime ) {
-			float fadeStart = fadebyTime - _fadeTime;
-			float fadeRatio = (Time.realtimeSinceStartup - fadeStart) / _fadeTime;
-
-			float alpha = _fadeCurve.Evaluate(fadeRatio);
-			SetAlpha(alpha);
-		} else {
-			SetAlpha(1f);
-			NextScene();
-		}
-	}
-
-	private void SetAlpha( float alpha ) {
-		_fadeOverlay.color = new Color(0f, 0f, 0f, alpha);
+	private void SceneChange() {
+		GameManager.LoadLevel(_sceneToLoad);
 	}
 
 	public void StartFade() {
 		_checkFadeDistance = true;
 	}
 
-	private void NextScene() {
-        GameManager.LoadLevel(_sceneToLoad);
-    }
 }
